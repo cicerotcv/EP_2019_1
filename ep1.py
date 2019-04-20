dcb14b3a4e60f2448680528185f931cdb172d32f
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-04-20 03:45:16 -0300

    corrigi alguns bugs

--- ep1.py
+++ ep1.py
@@ -24,7 +24,7 @@ def instrucoes():
         print(i)
 
 #Função Cenários
-def carregar_cenarios():
+def carregar_cenarios(nome):
     cenarios = {
         "inicio": {
             "titulo": "Saguao do perigo",
@@ -55,22 +55,25 @@ def carregar_cenarios():
     return cenarios, nome_cenario_atual
 
 # Função personagens
-def carregar_personagens():
+def carregar_personagens(nome):
     personagens = {
             "guarda":{
                     "descricao":"Brian, o guarda parceiro",
                     "saudacao":"Hey aluno, tudo bem? O que você quer?",
                     "opcoes": {
-                            "lutar":"lutar com o Brian.",
-                            "conversar": "conversar com o Brian."},
+                            "combate":"lutar com o Brian.",
+                            "conversar": "conversar com o Brian.",
+                            "voltar":"voltar para o saguão."}, # ainda não foi implementado
                     "imortal": True,
-                    "speech": "Olá, {0}, adoraria conversar, mas tenho que ver. "},
+                    "speech": "Olá, {0}, adoraria conversar, mas tenho que ver se ".format(nome)+ 
+                        "algum aluno está dormindo na biblioteca."},
             "professor": {
-                    "descricao": "O monstro do Python",
+                    "descricao": "o monstro do Python",
                     "saudacao": "Voce foi pedir para o professor adiar o EP."
                              "O professor revelou que é um monstro disfarçado "
                              "e devorou sua alma.",
-                    "opcoes": {}}
+                    "opcoes": {},
+                    "imortal":True}
             }
     return personagens
 
@@ -106,45 +109,54 @@ def main():
     print()
 
 
-    cenarios, nome_cenario_atual = carregar_cenarios()
-    personagens = carregar_personagens()
+    cenarios, condicao = carregar_cenarios(name)
+    personagens = carregar_personagens(name)
     game_over = False
 
 
     while not game_over:
         #Loading Inicial
-        if nome_cenario_atual in cenarios:
-            cenario_atual = cenarios[nome_cenario_atual]
+        if condicao in cenarios:
+            cenario_atual = cenarios[condicao]
             titulo = cenario_atual["titulo"]
             descricao = cenario_atual["descricao"]
             opcoes = cenario_atual['opcoes']
         else:
             cenario_atual = escolha
             titulo = personagens[escolha]["descricao"]
-            descricao = "Voce esta falando com {0}".format(titulo)
+            descricao = "Você está falando com {0}".format(titulo)
             opcoes = personagens[escolha]['opcoes']
-        
-        print(titulo,'\n{0}\n{1}\n'.format("-"*len(titulo),descricao))
-
+            
+            
+        print("{0}\n{1}\n{2}\n".format(titulo,"-"*len(titulo),descricao))
         
         if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
             game_over = True
         else:
-            #voltar = cenario_atual
             print('O que você vai fazer?')
             print()
             for opcao in opcoes:
                 print('{0}: {1}'.format(opcao,opcoes[opcao]))
-            escolha = input(">>>>> ")
-            if escolha == "prefiro pegar DP":
+            
+            
+            escolha = input(">>>>> ")  # palavra mágica do jogo
+            
+            
+            if escolha == "prefiro pegar DP": 
                 break
+            
             elif escolha in opcoes:
-                nome_cenario_atual = escolha
+                condicao = escolha
+            
             elif escolha in personagens:
                 print(personagens[escolha]["saudacao"])
-            elif escolha == 'monstro':
-                print(nome_inimigo_atual)
+            elif escolha == "voltar":
+                escolha = condicao
+                
+            elif escolha == 'combate':
+                print(condicao)
+            
             else:
                 print("Sua indecisão foi sua ruína!")
                 game_over = True
